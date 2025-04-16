44eb2f578a0808362654f7b5f693b7bf88661ae6
Kirill <k.ved2008@mail.ru>
2025-04-16 21:20:43 +0300

    delete i18

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,10 +26,6 @@ export default defineNuxtConfig({
     'dayjs-nuxt',
     '@nuxt/image',
     '@nuxtjs/color-mode',
-    '@nuxtjs/i18n',
     '@nuxt/ui'
-  ],
-  i18n: {
-    vueI18n: './i18n.config.ts'
-  }
+  ]
 })
